be2eb97eb536a838262627b593260f871012088a
Aayush <aayushgarg226@gmail.com>
2020-11-14 03:52:15 +0530

    popup shows and input word to replace

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,31 @@
 console.log("Content script running...");
 
 
-function replaceText(){
-    const FIND = "the";
-    const REPLACE_WITH = "replacely";
-    const regex = new RegExp(FIND, "gi");
+// function replaceText(){
+//     const FIND = "the";
+//     const REPLACE_WITH = "replacely";
+//     const regex = new RegExp(FIND, "gi");
+//     const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
+//     for(const snippet of contents){
+//             snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
+//     }
+//   }
+//   chrome.runtime.onMessage.addListener(function(message){
+//       if(message.action === 'REPLACE_TEXT'){
+//           replaceText();
+//       }
+//   });
+
+
+function replaceText(find, replace){
+    const regex = new RegExp(find, "gi");
     const contents = document.querySelectorAll('p,li,span,h1,h2,h3,h4,h5,h6');
     for(const snippet of contents){
-            snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
+          snippet.textContent = snippet.textContent.replace(regex, replace);
     }
-  }
-  chrome.runtime.onMessage.addListener(function(message){
-      if(message.action === 'REPLACE_TEXT'){
-          replaceText();
-      }
-  });
+}
+chrome.runtime.onMessage.addListener(function(message){
+    if(message.action === 'REPLACE_TEXT'){
+        replaceText(message.find, message.replace);
+    }
+});
